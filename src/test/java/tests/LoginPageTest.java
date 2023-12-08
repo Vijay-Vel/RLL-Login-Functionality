8f51fed429b099bfb66476be4c26dce34911ba4b
vijay <vijay@gmail.com>
2023-12-08 16:38:40 +0530

    Done

---
+++ src/test/java/tests/LoginPageTest.java
@@ -0,0 +1,64 @@
+package tests;
+
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import pages.HomePage;
+import pages.LoginPage;
+import pages.TestBase;
+import utilities.ExcelUtility;
+import utilities.MyListener;
+
+
+@Listeners(MyListener.class)
+public class LoginPageTest extends TestBase {
+	
+	HomePage hp;
+	LoginPage lp;
+	public static int RowNum=1;
+	@BeforeTest
+	public void start_browser()
+	{
+		OpenBrowser();
+		hp = new HomePage(driver);
+		lp = new LoginPage(driver);
+	}
+
+	
+	@Test(dataProvider="LoginDetails")
+	public void test_login(String email, String password) throws IOException, InterruptedException
+	{
+		hp.clickLogin();
+		lp.user_login(email,password);
+		String uname=lp.get_uname();
+		
+		Assert.assertEquals(uname, email);
+		lp.user_logout();
+		RowNum=RowNum+1;
+	}
+	
+	
+	  @DataProvider(name="LoginDetails") public Object[][] datasupplier() throws
+	  EncryptedDocumentException, IOException {
+	  
+	  Object[] [] input = ExcelUtility.getTestData("Sheet1"); return input;
+	  
+	  }
+	  
+
+	@AfterTest
+	public void close_browser()
+	{
+		driver.close();
+	}
+	
+	
+
+}
